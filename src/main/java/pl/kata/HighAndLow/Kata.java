52fd2d65a563f32f4b99a66f00fab87c794b6e6b
Krzysztof Muller <krzysztofmuller1@gmail.com>
2018-01-24 21:10:49 +0100

    highest and lowest ints

--- src/main/java/pl/kata/HighAndLow/Kata.java
+++ src/main/java/pl/kata/HighAndLow/Kata.java
@@ -7,32 +7,57 @@ public class Kata {
 
     public static String HighAndLow(String numbers) {
         // Code here or
-
-//        int min = Arrays.stream(numbers.split(" "))
-//                .mapToInt(i -> Integer.parseInt(i))
-//                .min()
-//                .getAsInt();
-//
-//        int max = Arrays.stream(numbers.split(" "))
-//                .mapToInt(i -> Integer.parseInt(i))
-//                .max()
-//                .getAsInt();
-//
-//        return max + " " + min;
-
         IntSummaryStatistics summary = Arrays.stream(numbers.split(" "))
                 .mapToInt(i -> Integer.parseInt(i))
                 .summaryStatistics();
 
-//        return summary.getMax() + " " + summary.getMin();
         String some = "krzys";
-        double ff =  3.3;
-        return String.format("%d %d %s, %.2f" , summary.getMax(), summary.getMin(), some, ff);
+        double ff = 3.3;
+//        return summary.getMax() + " " + summary.getMin();
+        return String.format("%d %d %s, %.2f", summary.getMax(), summary.getMin(), some, ff);
+    }
+
+    public static String HighAndLow2(String numbers) {
+
+        int min = Arrays.stream(numbers.split(" "))
+                .mapToInt(i -> Integer.parseInt(i))
+                .min()
+                .getAsInt();
+
+        int max = Arrays.stream(numbers.split(" "))
+                .mapToInt(i -> Integer.parseInt(i))
+                .max()
+                .getAsInt();
+
+        return max + " " + min;
+    }
+
+    public static String HighAndLow3(String numbers) {
+        String[] str = numbers.split(" ");
+        Integer[] ints = new Integer[str.length];
+
+        for (int i = 0; i < str.length; i++) {
+            ints[i] = Integer.parseInt(str[i]);
+        }
+        int min = ints[0];
+        int max = ints[0];
+
+        for (int i : ints) {
+            if (max < i)
+                max = i;
+            if (min > i)
+                min = i;
+        }
+        return max + " " + min;
     }
 
 
     public static void main(String[] args) {
         String a = HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
         System.out.println(a);
+
+        String b = HighAndLow3("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
+        System.out.println(b);
+
     }
 }
