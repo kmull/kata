a6a995e1b0919a468bd0327ccb1657d165f8d148
Krzysztof Muller <krzysztofmuller1@gmail.com>
2018-04-29 17:11:43 +0200

    compare Integer

--- src/main/java/paczka/nauka/porownaj/MainClass.java
+++ src/main/java/paczka/nauka/porownaj/MainClass.java
@@ -4,27 +4,50 @@ public class MainClass {
 
     public static void main(String[] args) {
 
-        String a = "abc";
-        String b = "abc";
-        String c = "abd";
-        String aabb = new String("abc");
+//        String a = "abc";
+//        String b = "abc";
+//        String c = "abd";
+//        String aabb = new String("abc");
+//
+//        String aa = new String("ccc");
+//        String bb = new String("ccc");
+//
+//        System.out.println(a == b);       // true
+//        System.out.println(a == aabb);    // false
+//        System.out.println(aa == bb);     // false
+//        System.out.println(a == c);       // false
+//        System.out.println();
+//
+//        System.out.println(a.equals(b));     // true
+//        System.out.println(a.equals(c));     // false
+//        System.out.println(aa.equals(aabb)); // false
+//        System.out.println(a.equals(aabb));  // true
 
-        String aa = new String("ccc");
-        String bb = new String("ccc");
+        Integer i1 = 10;
+        Integer i2 = 10;
+        Integer i11 = new Integer(10);
+        Integer i12 = new Integer(10);
 
-        System.out.println(a == b);
-        System.out.println(a == aabb);
-        System.out.println(aa == bb);
-        System.out.println(a == c);
+        System.out.println("i1/i2");
+        System.out.println(i1 == i2);       // true
+        System.out.println(i1.equals(i2));  // true
         System.out.println();
 
-        System.out.println(a.equals(b));
-        System.out.println(a.equals(c));
-        System.out.println(aa.equals(aabb));
-        System.out.println(a.equals(aabb));
+        System.out.println(i1 == i11);      // false
+        System.out.println(i1.equals(i11)); // true
+        System.out.println();
 
-//        Integer i1 = 10;
-//        Integer i2
+        System.out.println(i11 == i12);      // false
+        System.out.println(i11.equals(i12)); // true
+        System.out.println();
+
+        System.out.println("hashcode: ");
+        System.out.println(i1.hashCode());
+        System.out.println(i2.hashCode());
+        System.out.println(i11.hashCode());
+        System.out.println(i12.hashCode());
+//        String gg = String.valueOf(10);
+        System.out.println(String.valueOf(10).hashCode());
 
     }
 
