58e6749e092a317d47a2e8e1488702c8c42cd32c
Krzysztof Muller <krzysztofmuller1@gmail.com>
2018-04-30 19:04:59 +0200

    stream learning2

--- src/main/java/java8/youtube/stream1/MyStreamWithMap.java
+++ src/main/java/java8/youtube/stream1/MyStreamWithMap.java
@@ -2,18 +2,30 @@ package java8.youtube.stream1;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyStreamWithMap {
 
     public static void main(String[] args) {
 
-        List<String> people = Arrays.asList("Ala", "Ania", "Armin", "Tomek", "Wojciech", "Daria", "Wiola", "Michal");
+        List<String> people = Arrays.asList("Ala", "Alo", "Ania", "Armin", "Tomek", "Wojciech", "Daria", "Wiola", "Michal");
+//
+//        people
+//                .stream()
+////                .map(x -> x.toLowerCase())
+//                .map(String::toLowerCase)
+//                .filter(x -> x.startsWith("a"))
+//                .forEach(System.out::println);
 
-        people
-                .stream()
-//                .map(x -> x.toLowerCase())
+//        people.stream()
+//                .sorted()
+//                .filter(x -> x.length() < 6)
+//                .forEach(System.out::println);
+
+        List<String> myPeople = people.stream()
                 .map(String::toLowerCase)
-                .filter(x -> x.startsWith("a"))
-                .forEach(System.out::println);
+                .filter(x -> x.contains("a"))
+                .collect(Collectors.toList());
+        myPeople.forEach(x -> System.out.println(x));
     }
 }
